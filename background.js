d16d17c36d40afcf94250c4023b641a3496b18c5
Baris Balic <baris.balic@forward.co.uk>
2013-05-13 21:34:43 +0100

    Reworded README slightly. Removed redundant URL check in background.js. Dropped redundant permissions and wildcarded the .xbox.com domains in manifest.json

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 function replaceMspWithCost(tabId, changeInfo, tab) {
-  chrome.pageAction.show(tabId);
+  if (tab.url.indexOf('marketplace.xbox.com') > -1) {
+    chrome.pageAction.show(tabId);
+  }
 };
 
 // Listen for any changes to the URL of any tab.
